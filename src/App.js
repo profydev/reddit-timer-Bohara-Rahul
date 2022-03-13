115f7c1a78762cdd2284531eee438e3e33255614
Rahul <rahul.bohara2016@gmail.com>
2022-03-13 23:51:33 +1000

    Added routes for home and search pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import SearchPage from './pages/SearchPage';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </Router>
   );
 }
 
