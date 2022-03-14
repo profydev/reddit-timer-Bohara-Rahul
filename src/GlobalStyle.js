6850c72cbe7de13a2029dec88a417e11cae395c3
Rahul <rahul.bohara2016@gmail.com>
2022-03-14 12:25:19 +1000

    Fixed semicolon and single quote issues

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,7 +1,7 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 
 const globalStyles = createGlobalStyle`
     
-`
+`;
 
-export default globalStyles
+export default globalStyles;
